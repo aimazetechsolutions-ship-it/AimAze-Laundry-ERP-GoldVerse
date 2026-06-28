c3a206183848470fe4aed927d32ec926d0136d59
AimAze Tech Solutions <aimazetechsolutions-ship-it@users.noreply.github.com>
2026-06-29 00:49:03 +0400

    Block typing + hide Save on locked customer form for non-admins

--- addons/goldverse_premium_laundry_branding/static/src/js/goldverse_partner_lock.js
+++ addons/goldverse_premium_laundry_branding/static/src/js/goldverse_partner_lock.js
@@ -2,6 +2,7 @@
 
 import { patch } from "@web/core/utils/patch";
 import { FormController } from "@web/views/form/form_controller";
+import { useEffect } from "@odoo/owl";
 
 const PROTECTED_ITEMS = new Set([
     "archive",
@@ -10,7 +11,35 @@ const PROTECTED_ITEMS = new Set([
     "duplicate",
 ]);
 
+function applyLockClass(rootEl, locked) {
+    if (!rootEl) return;
+    rootEl.classList.toggle("gv-customer-locked-form", !!locked);
+}
+
 patch(FormController.prototype, {
+    setup() {
+        super.setup();
+        useEffect(
+            () => {
+                if (this.props.resModel !== "res.partner") return;
+                const locked = !!(
+                    this.model.root &&
+                    this.model.root.data &&
+                    this.model.root.data.goldverse_partner_is_locked
+                );
+                const root = document.querySelector(".o_form_view") || document.querySelector(".o_form_renderer");
+                applyLockClass(root, locked);
+            },
+            () => [
+                this.props.resModel,
+                this.model.root && this.model.root.data
+                    ? this.model.root.data.goldverse_partner_is_locked
+                    : null,
+                this.model.root && this.model.root.resId,
+            ]
+        );
+    },
+
     getStaticActionMenuItems() {
         const items = super.getStaticActionMenuItems(...arguments);
         try {
